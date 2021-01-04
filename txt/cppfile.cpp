110ff96fb89c3b81e9e16c1816f00f8f4efa2ac5
kheirkhah9821160014mohammadmahdi <kheirkhah.mahdi.9821160014@gmail.com>
2021-01-04 10:10:35 +0330

    cpp

---
+++ txt/cppfile.cpp
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+	int i;
+	ofstream f1("2.out.txt");
+	for (i =0 ; i<10 ;i = i+2)
+		f1 << i << endl;
+	f1<< "mahdi kheirkhah" << endl;
+	f1.close();	
+	return 0;
+}
+
